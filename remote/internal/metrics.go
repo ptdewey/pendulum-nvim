52215b158f2f482444acd1f23ea2266b29f5402a
ptdewey <patrickdewey2002@gmail.com>
2024-11-18 16:12:42 -0500

    Revert "feat: custom popup buffer name" -- names need to be unique
    This reverts commit 8853494b124bdc71aadeb6b85fa4a7d750395e1c.

--- remote/internal/metrics.go
+++ remote/internal/metrics.go
@@ -210,8 +210,6 @@ func aggregatePendulumMetric(
 
 		pv.TotalTime += t
 
-		// TODO: common active time aggregation here?
-
 		// active-only metrics aggregation
 		if active == true {
 			pv.ActiveCount++
