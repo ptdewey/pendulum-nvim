bfc80e3e86359b5dac3a6755b049fed17e5e99f6
ptdewey <patrickdewey2002@gmail.com>
2025-03-27 21:50:57 -0400

    feat: percentage in hours view

--- remote/internal/prettify/hours.go
+++ remote/internal/prettify/hours.go
@@ -6,29 +6,29 @@ import (
 	"pendulum-nvim/pkg/args"
 	"sort"
 	"time"
+	"unicode/utf8"
 )
-
-type hourFreq struct {
-	hour  int
-	count int
-}
-
-func PrettifyActiveHours(hours *data.PendulumHours) []string {
-	pendulumArgs := args.PendulumArgs()
-	return []string{prettifyActiveHours(hours, pendulumArgs.NHours,
-		pendulumArgs.TimeFormat, pendulumArgs.TimeZone)}
-}
 
 type hourDuration struct {
 	hour     int
 	duration time.Duration
 }
 
+func PrettifyActiveHours(hours *data.PendulumHours) []string {
+	pendulumArgs := args.PendulumArgs()
+	return []string{
+		prettifyActiveHours(hours, pendulumArgs.NHours,
+			pendulumArgs.TimeFormat, pendulumArgs.TimeZone),
+	}
+}
+
 func prettifyActiveHours(hours *data.PendulumHours, n int, timeFormat string, timeZone string) string {
-	hourCounts := make(map[int]int)
-	hourDurations := make(map[int]time.Duration)
-	weekHourDurations := make(map[int]time.Duration)
-	totalCount := 0
+	hourCountsActive := make(map[int]int)
+
+	hourDurationsActive := make(map[int]time.Duration)
+	hourDurationsTotal := make(map[int]time.Duration)
+	weekHourDurationsActive := make(map[int]time.Duration)
+	weekHourDurationsTotal := make(map[int]time.Duration)
 
 	loc, err := time.LoadLocation(timeZone)
 	if err != nil {
@@ -45,23 +45,33 @@ func prettifyActiveHours(hours *data.PendulumHours, n int, timeFormat string, ti
 			continue
 		}
 
-		hourCounts[t.In(loc).Hour()]++
-		totalCount++
+		hourCountsActive[t.In(loc).Hour()]++
 	}
 
 	for k, v := range hours.ActiveTimeHours {
 		t := time.Date(2006, 1, 2, k, 0, 0, 0, time.UTC)
-		hourDurations[t.In(loc).Hour()] += v
+		hourDurationsActive[t.In(loc).Hour()] += v
+
+	}
+
+	for k, v := range hours.TotalTimeHours {
+		t := time.Date(2006, 1, 2, k, 0, 0, 0, time.UTC)
+		hourDurationsTotal[t.In(loc).Hour()] += v
 	}
 
 	for k, v := range hours.ActiveTimeHoursRecent {
 		t := time.Date(2006, 1, 2, k, 0, 0, 0, time.UTC)
-		weekHourDurations[t.In(loc).Hour()] += v
+		weekHourDurationsActive[t.In(loc).Hour()] += v
+	}
+
+	for k, v := range hours.TotalTimeHoursRecent {
+		t := time.Date(2006, 1, 2, k, 0, 0, 0, time.UTC)
+		weekHourDurationsTotal[t.In(loc).Hour()] += v
 	}
 
 	// Create a slice of hourDuration structs to sort by duration
 	var hourDurationSlice []hourDuration
-	for hour, duration := range hourDurations {
+	for hour, duration := range hourDurationsActive {
 		hourDurationSlice = append(hourDurationSlice, hourDuration{hour: hour, duration: duration})
 	}
 
@@ -74,26 +84,40 @@ func prettifyActiveHours(hours *data.PendulumHours, n int, timeFormat string, ti
 		n = len(hourDurationSlice)
 	}
 
+	var overallHoursWidth int
+	var recentHoursWidth int
+
+	for _, d := range hourDurationsActive {
+		w := utf8.RuneCountInString(fmt.Sprintf("%v", d))
+		if overallHoursWidth < w {
+			overallHoursWidth = w
+		}
+	}
+
+	for _, d := range weekHourDurationsActive {
+		w := utf8.RuneCountInString(fmt.Sprintf("%v", d))
+		if recentHoursWidth < w {
+			recentHoursWidth = w
+		}
+	}
+
 	// Column width stuff
 	bulletWidth := len(fmt.Sprintf("%d", n))
-	timeWidth := 8
-	overallWidth := 13
-	weeklyWidth := 13
-	countWidth := 3
+	// TODO: store largest duration string lengths
 
 	// Header formatting
 	out := fmt.Sprintf("# Times Most Active\n")
 	out += fmt.Sprintf("%*s  %-*s %-*s %-*s %-*s\n",
-		bulletWidth, "", timeWidth, "Time", overallWidth, "Overall",
-		weeklyWidth, "This Week", countWidth, "Entry Count",
+		bulletWidth, "", 7, "Time", 21, "Overall (Active %)",
+		23, "This Week (Active %)", 3, "Entry Count",
 	)
 
 	// Loop through results and format accordingly
 	for i := range n {
 		h24 := hourDurationSlice[i].hour
-		c := hourCounts[h24]
-		dur := hourDurations[h24]
-		weeklyDur := weekHourDurations[h24]
+		c := hourCountsActive[h24]
+		dur := hourDurationsActive[h24]
+		weeklyDur := weekHourDurationsActive[h24]
 
 		h := h24
 		var period string
@@ -108,14 +132,17 @@ func prettifyActiveHours(hours *data.PendulumHours, n int, timeFormat string, ti
 			}
 		}
 
-		// TODO: show percentage active time (out of total)
-		out += fmt.Sprintf("%*d. %2d%s %-*s %-*v %-*v %-*d (%.2f%%)\n",
+		out += fmt.Sprintf("%*d. %2d%s %-*s%-*v (%.2f%%)%-*s %-*v (%.2f%%)%-*s %-*d\n",
 			bulletWidth, i+1,
 			h, period,
-			timeWidth-5, "",
-			overallWidth, dur,
-			weeklyWidth, weeklyDur,
-			countWidth, c, float64(c)/float64(totalCount)*100,
+			3, "",
+			overallHoursWidth, dur,
+			float64(dur)/float64(hourDurationsTotal[h24])*100,
+			3, "",
+			recentHoursWidth, weeklyDur,
+			float64(weeklyDur)/float64(weekHourDurationsTotal[h24])*100,
+			6, "",
+			3, c,
 		)
 	}
 
